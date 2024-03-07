0bba4386504f8efd017f513427ef8de855604e56
Mauricio Yukio Tonooka <mytonooka@MacBook-Air-de-Mauricio.local>
2024-03-06 23:37:08 -0300

    versao 2 - Aula JPA - Consulta Tab e delete

--- src/main/java/aplicativo/Programa.java
+++ src/main/java/aplicativo/Programa.java
@@ -11,22 +11,19 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
+		Pessoa p = em.find(Pessoa.class, 1);		
+		
+		//System.out.println(p);
+		// (errado) Pessoa p = new Pessoa (2,null,null);
+		
 		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
+		em.remove(p);
 		em.getTransaction().commit();
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println("Registro deletado com sucesso!!");
 		
 		em.close();
 		emf.close();
